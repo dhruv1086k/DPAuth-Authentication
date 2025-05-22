f4fe5c3a26ea440dd0468921fdec512e070bb2f6
Dhruv Pal <dhruvpal.0905@gmail.com>
2025-05-22 23:28:05 +0530

    add controller function for resetting user password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -254,3 +254,46 @@ export const sendResetOtp = async (req, res) => {
     return res.json({ success: false, message: err.message });
   }
 };
+
+// ============================== controller function for resetting user password ================================
+export const resetPassword = async (req, res) => {
+  const { email, otp, newPassword } = req.body;
+
+  if (!email || !otp || !newPassword) {
+    return res.json({
+      success: false,
+      message: "Email, OTP & new password are required",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    if (user.resetOtp === "" || user.resetOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.resetOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP expired" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
+    user.resetOtp = "";
+    user.resetOtpExpireAt = 0;
+
+    await user.save();
+
+    return res.json({
+      success: true,
+      message: "Password has reset successfully",
+    });
+  } catch (err) {
+    return res.json({ success: false, message: err.message });
+  }
+};
