e9f14fb91d342ac917fd5f23bef74f2056a2aee2
Dhruv Pal <dhruvpal.0905@gmail.com>
2025-05-22 23:49:52 +0530

    add user endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,6 +4,8 @@ import {
   login,
   logout,
   register,
+  resetPassword,
+  sendResetOtp,
   sendVerifyOtp,
   verifyEmail,
 } from "../controllers/authController.js";
@@ -32,5 +34,11 @@ authRouter.post("/is-auth", userAuth, isUserAuthenticated);
 // This route serves as an authentication status checker.
 // userAuth middleware will run and it will give {success: true} only if we are using correct token
 // isUserAuthenticated will work only when userAuth will be passed and isUserAuthenticated will only show a {success: true}
+
+// --------   /api/auth/send-reset-otp   --------
+authRouter.post("/send-reset-otp", sendResetOtp);
+
+// --------   /api/auth/reset-password  --------
+authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -19,7 +20,11 @@ app.get("/", (req, res) => {
   res.send("working");
 });
 
+// auth routes
 app.use("/api/auth", authRouter);
+
+// user routes
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
